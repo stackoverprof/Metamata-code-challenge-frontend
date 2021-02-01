db2e68edf4c999b882c4ae9ffa5c1303a39b610b
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-02-02 01:59:32 +0700

    Hero and searchbar done

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,58 +1,25 @@
 import React from 'react'
 import { css } from '@emotion/react'
-import Link from 'next/link'
 import useResize from 'use-resizing'
-import { useAuth } from '../core/contexts/AuthContext'
-import to from '../core/routepath'
 
 import HomeLayout from '../components/layouts/HomeLayout'
 import Hero from '../components/molecular/Hero'
+import SearchBar from '../components/molecular/SearchBar'
+import AuthLinks from '../components/molecular/AuthLinks'
     
 const Home = () => {
     const screen = useResize().width
-    const { authState } = useAuth()
 
     return (
-        <HomeLayout style={style({screen})}>
+        <HomeLayout style={style({screen})} className="flex-cc-col">
             <Hero />
-            <div className="links">
-                { authState === 'guest' && <Link href={to.login}><button>Login</button></Link> }
-                { authState === 'guest' && <Link href={to.register}><button>Register</button></Link> }
-                { authState === 'user' && <Link href={to.dashboard}><button>Dashboard</button></Link> }
-            </div>
+            <SearchBar />
+            <AuthLinks />
         </HomeLayout>
     )
 }
 
 const style = ({screen}) => css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-
-    h1{
-        font-size: 54px;
-        margin: 120px 0 8px 0;
-    }
-
-    h2{
-        font-size: 16px;
-        font-weight: bold;
-    }
-
-    p{
-        margin-bottom: 32px;
-        max-width: 520px;
-        width: 90%;
-        min-width: 320px;
-        text-align: center;
-    }
-    .links{
-        margin-top: 24px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
 
 `
     
