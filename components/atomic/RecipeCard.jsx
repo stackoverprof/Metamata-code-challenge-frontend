4e6d5aa9f382a1e3b82de9a2275dd3a9403b7c1f
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-02-02 21:48:32 +0700

    moving letter 1

--- components/atomic/RecipeCard.jsx
+++ components/atomic/RecipeCard.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { css } from '@emotion/react'
 
 const RecipeCard = ({title, imageURL}) => {
+    const [mover, setMover] = useState(false)
+    const ref = useRef(null)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setMover(true)
+        }, 3000)
+    }, [])
 
     return (
-        <div css={style}>
+        <div css={style({mover, ref})}>
             <img src={imageURL} alt=""/>
-            <div className="content flex -bc">
-                <p>{title}</p>
+            <div className="content">
+                <div className="title flex -sc">
+                    <p className="flex -cc" ref={ref}>{title}</p>
+                </div>
                 <button className="btn-green">favorit</button>
             </div>
         </div>
     )
 }
 
-const style = css`
+const style = ({mover, ref}) => css`
     display: block;
     width: 100%;
     background: #3A3C42;
@@ -23,6 +33,7 @@ const style = css`
     box-shadow: 0 0 4px #0005;
 
     .content{
+        position: relative;
         height: 40px;
         font-family: Roboto;
         font-weight: bold;
@@ -34,6 +45,8 @@ const style = css`
         color: #9CCD62;
         
         button{
+            position: absolute;
+            right: 12px;
             background: #9CCD62;
             border-radius: 10px;
             padding: 8px 12px;
@@ -42,11 +55,24 @@ const style = css`
             margin: 0;
         }
 
-        p{
-            max-width: calc(100% - 78px);
-            overflow: hidden;
+        .title{
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
             margin-right: 4px;
+
+            p{
+                position: absolute;
+                transition: 1s;
+                top: 0;
+                margin-left: 12px;
+                height: 100%;
+                left: ${!mover ? '0' : `-${ref.current.offsetWidth - 178 }px`};
+            }
         }
+
         img{
             margin: 0;
         }
