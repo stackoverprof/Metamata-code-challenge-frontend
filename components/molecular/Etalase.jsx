d7478abd7d86ea6cf252bc82dd22a42b74261e36
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-02-03 12:06:03 +0700

    resposivity grid done

--- components/molecular/Etalase.jsx
+++ components/molecular/Etalase.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { css } from '@emotion/react'
 import RecipeCard from '@components/atomic/RecipeCard'
+import useResize from 'use-resizing'
 
 const Etalase = ({data}) => {
+    const screen = useResize().width
 
     return (
         <section css={style} className="flex -cc -col">
             <p className="header">Hasil Pencarian Resep</p>
-            <div className="contain-size--m">
+            <div className={`contain-size--m ${screen <= 616 ? 'maw-300' : ''} ${data && data.length === 1 ? 'maw-300' :''} ${data && data.length === 2 ? 'maw-600' :''}`}>
                 <div className="gridder">
                     {data?.map((item) => (
                         <RecipeCard key={item.id} data={item}/>
@@ -20,11 +22,19 @@ const Etalase = ({data}) => {
 }
 
 const style = css`
+
     .gridder{
         display: grid;
         grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
         gap: 20px;
         padding: 20px;
+    }
+    
+    .maw-300{
+        max-width: 300px;
+    }
+    .maw-600{
+        max-width: 600px;
     }
 
     p.header{
